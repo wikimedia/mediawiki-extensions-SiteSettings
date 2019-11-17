cde1583234e556b747c85288c1a74c55272c86cc
Zoranzoki21 <zorandori4444@gmail.com>
2019-11-17 16:09:17 +0000

    Add aliases for Bosnian, Croatian and Serbian
    Change-Id: Ia4b6c500fc3edfe7c8764333e2324ce4c58a2ce2

--- SiteSettings.alias.php
+++ SiteSettings.alias.php
@@ -12,3 +12,23 @@ $specialPageAliases = array();
 $specialPageAliases['en'] = array(
 	'SiteSettings' => array( 'SiteSettings' ),
 );
+
+/** Bosnian (bosanski) */
+$specialPageAliases['bs'] = array(
+	'SiteSettings' => array( 'PostavkeSajta' ),
+);
+
+/** Croatian (hrvatski) */
+$specialPageAliases['hr'] = array(
+	'SiteSettings' => array( 'Postavke_web_mjesta' ),
+);
+
+/** Serbian Cyrillic (српски (ћирилица)) */
+$specialPageAliases['sr-ec'] = array(
+	'SiteSettings' => array( 'ПодешавањаСајта' ),
+);
+
+/** Serbian Latin (srpski (latinica)) */
+$specialPageAliases['sr-el'] = array(
+	'SiteSettings' => array( 'PodešavanjaSajta' ),
+);
